21d0ad3fe46f09c9ee8c0d1886f7eaeddad20fdf
Jason Ball <jasonericball@gmail.com>
2020-12-02 15:41:48 -0500

    Add demo content

---
+++ pages/demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="quiz in $quizzes" :key="quiz">
+      <Quiz :quiz="quiz" />
+      <br />
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+import Quiz from '../components/Quiz.vue'
+export default {
+  name: 'Demo',
+  components: {
+    Quiz,
+  },
+  auth: false,
+}
+</script>
+
+<style></style>
